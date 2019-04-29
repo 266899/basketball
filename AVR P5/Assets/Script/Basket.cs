c753792e52ea10e3260949311bd09f17978b23f2
2 <266899@via.dk>
2019-04-29 20:24:57 +0200

    init

---
+++ AVR P5/Assets/Script/Basket.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Basket : MonoBehaviour
+{
+    public GameObject score; //reference to the ScoreText gameobject, set in editor
+
+
+    
+    void OnTriggerEnter() //if ball hits basket collider
+    {
+        int currentScore = int.Parse(score.GetComponent<Text>().text) + 1; //add 1 to the score
+        score.GetComponent<Text>().text = currentScore.ToString();
+      
+         
+       
+    }
+}
---
+++ AVR P5/Assets/VRTK/Assets/VRTK/Examples/ExampleResources/SceneResources/[005 - Interactions] InteractableObjects/Scripts/ControllableReactor.cs
@@ -0,0 +1,47 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+    using VRTK.Controllables;
+
+    public class ControllableReactor : MonoBehaviour
+    {
+        public GameObject wall;
+        public VRTK_BaseControllable controllable;
+        public Text displayText;
+        public string outputOnMax = "Maximum Reached";
+        public string outputOnMin = "Minimum Reached";
+
+        protected virtual void OnEnable()
+        {
+            controllable = (controllable == null ? GetComponent<VRTK_BaseControllable>() : controllable);
+            controllable.ValueChanged += ValueChanged;
+            controllable.MaxLimitReached += MaxLimitReached;
+            controllable.MinLimitReached += MinLimitReached;
+        }
+
+        protected virtual void ValueChanged(object sender, ControllableEventArgs e)
+        {
+            if (displayText != null)
+            {
+                displayText.text = e.value.ToString("F1");
+            }
+        }
+
+        protected virtual void MaxLimitReached(object sender, ControllableEventArgs e)
+        {
+            if (outputOnMax != "")
+            {
+                wall.SetActive(false);
+            }
+        }
+
+        protected virtual void MinLimitReached(object sender, ControllableEventArgs e)
+        {
+            if (outputOnMin != "")
+            {
+                Debug.Log(outputOnMin);
+            }
+        }
+    }
+}
